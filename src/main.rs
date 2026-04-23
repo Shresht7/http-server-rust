cfc1af4b2f8d01cd328a6bc767ba0757ab688539
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-23 19:12:21 +0530

    Bind TCP Listener and Accept Connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use std::net;
+use std::process;
+
+/// The network address host to listen on
+const ADDRESS_HOST: &str = "127.0.0.1";
+
+/// The network address port to listen on
+const ADDRESS_PORT: u16 = 8080;
+
+/// The main entry point of the application
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
+}
+
+/// Runs the HTTP server
+fn run() -> io::Result<()> {
+    // Bind the TCP listener to the specified address and port
+    let listener = net::TcpListener::bind((ADDRESS_HOST, ADDRESS_PORT))?;
+    println!("Server Listening on: {}:{}", ADDRESS_HOST, ADDRESS_PORT);
+
+    // Listen for incoming connections...
+    for stream in listener.incoming() {
+        let stream = stream?;
+        let recv_addr = stream.local_addr()?;
+        println!("Received connection from {}", recv_addr);
+    }
+
+    Ok(())
 }
